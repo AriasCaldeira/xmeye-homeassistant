625163effd28b05924336713713b287fee60d22b
AriasCaldeira <131480303+AriasCaldeira@users.noreply.github.com>
2025-07-07 19:26:37 -0300

    Update __init__.py

--- custom_components/xmeye/__init__.py
+++ custom_components/xmeye/__init__.py
@@ -1,17 +1,21 @@
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: dict):
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType):
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    # ✅ CORRETO: chamando a função diretamente de `hass`
-    hass.async_create_task(
-        await hass.config_entries.async_forward_entry_setups(entry, ["camera"])
-    )
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setups(entry, ["camera"])
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    return await hass.config_entries.async_forward_entry_unload(entry, "camera")
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, ["camera"])
